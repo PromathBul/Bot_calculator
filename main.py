8f34ba6bffb9816773bc231d8c180305a0487f03
Dmitry_Tikhonov <tdmv1209@gmail.com>
2022-12-02 19:03:24 +0300

    Удалил токены

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from telegram import Bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from controller import *
 
-bot = Bot(token = '5900644164:AAGoh9XW1-JH6yTNJfv9MAgslmkyJLEZHv8')
-updater = Updater(token = '5900644164:AAGoh9XW1-JH6yTNJfv9MAgslmkyJLEZHv8')
+bot = Bot(token = '')
+updater = Updater(token = '')
 dispatcher = updater.dispatcher
 
 start_handler = CommandHandler('start', start)
